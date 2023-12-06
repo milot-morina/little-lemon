e1b53dad6673a7c4eb8a175f51cee7bd348aa21d
milot-morina <milot.m3@gmail.com>
2023-12-06 12:31:07 +0100

    Added the neccessary assets and developed the navbar and Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Header from './components/Header';
+import './components/Nav'
+import Nav from './components/Nav';
 
 function App() {
   return (
     <>
-    <h1>Hello from boilerplate!</h1>
+      <Nav/>
+      <Header/>
     </>
   );
 }
